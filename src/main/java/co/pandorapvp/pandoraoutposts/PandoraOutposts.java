3e9ab8ac78288c6f626622dd8c498143a3c24dd1
Daniel <danieldawit33@gmail.com>
2022-05-25 16:40:26 -0400

    Initial commit

---
+++ src/main/java/co/pandorapvp/pandoraoutposts/PandoraOutposts.java
@@ -0,0 +1,18 @@
+package co.pandorapvp.pandoraoutposts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PandoraOutposts extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
+
