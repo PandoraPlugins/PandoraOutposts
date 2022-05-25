01930254a0ea20a5f0d061adcdf621240e462238
Daniel <danieldawit33@gmail.com>
2022-05-25 17:33:28 -0400

    add outpost flag

--- src/main/java/co/pandorapvp/pandoraoutposts/PandoraOutposts.java
+++ src/main/java/co/pandorapvp/pandoraoutposts/PandoraOutposts.java
@@ -1,18 +1,61 @@
 package co.pandorapvp.pandoraoutposts;
 
+import co.pandorapvp.pandoraoutposts.events.RegionEvents;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.StringFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import me.nanigans.libnanigans.Files.YamlGenerator;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+
 public final class PandoraOutposts extends JavaPlugin {
+
+    public WorldGuardPlugin worldGuardPlugin = getWorldGuard();
+    public Flag outpostFlag;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new RegionEvents(), this);
+        try {
+            YamlGenerator.createFolder(this, "Outposts");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onLoad() {
+
+        final FlagRegistry flagRegistry = worldGuardPlugin.getFlagRegistry();
+        try{
+            final StringFlag isOutpostFlag = new StringFlag("is-outpost", "true");
+            flagRegistry.register(isOutpostFlag);
+        }catch(FlagConflictException e){
+            e.printStackTrace();
+            Flag<?> existing = flagRegistry.get("is-outpost");
+            if(existing instanceof StateFlag){
+                outpostFlag = existing;
+            }else{
+                Bukkit.getConsoleSender().sendMessage(ChatColor.BOLD+""+ChatColor.RED+"Getting this error is bad news for the OUTPOST plugin, something is conflicing with the 'is-outpost' flag in worldguard");
+            }
+        }
+
     }
+
+    public WorldGuardPlugin getWorldGuard(){
+        Plugin plugin = this.getServer().getPluginManager().getPlugin("WorldGuard");
+        if(!(plugin instanceof WorldGuardPlugin)) return null;
+        return (WorldGuardPlugin) plugin;
+    }
+
 }
 
---
+++ src/main/java/co/pandorapvp/pandoraoutposts/events/RegionEvents.java
@@ -0,0 +1,17 @@
+package co.pandorapvp.pandoraoutposts.events;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import com.mewin.WGRegionEvents.events.RegionEnterEvent;
+
+public class RegionEvents implements Listener {
+
+    @EventHandler
+    public void onRegionEnter(RegionEnterEvent event){
+
+
+
+    }
+
+}
